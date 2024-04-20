e4cc8d8c4ff083f4c6f6edf70ca5f4da6ba75789
JvTechGuy <Alvinjendrey@gmail.com>
2024-04-20 21:55:22 +0800

    Updated to version Messenger 452.0.0.50.109

--- app/src/main/java/tn/amin/mpro2/constants/ModuleInfo.java
+++ app/src/main/java/tn/amin/mpro2/constants/ModuleInfo.java
@@ -6,9 +6,9 @@ public class ModuleInfo {
     public static final String LINK_TELEGRAM_SUPPORT = "https://t.me/MProSupport/";
     public static final String LINK_GITHUB_WIKI_USAGE_GUIDE = "https://github.com/Mino260806/MessengerPro/wiki/Usage-Guide/";
 
-    public static final int MIN_ORCA_VERSION = 320210038;
-    public static final int MAX_ORCA_VERSION = 320210121;
-    public static final String RECOMMENDED_ORCA_VERSION_STRING = "451.1.0.59.109";
+    public static final int MIN_ORCA_VERSION = 320409631;
+    public static final int MAX_ORCA_VERSION = 320608578;
+    public static final String RECOMMENDED_ORCA_VERSION_STRING = "452.0.0.50.109";
 
     public static final String PACKAGE_AI_PLUGIN = "tn.amin.mproai";
 }
