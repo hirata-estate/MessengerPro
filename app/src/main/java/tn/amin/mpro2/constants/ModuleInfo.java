8311f6264f8d7e9e846d0c227fd5ccaaa295eea8
Matt Rinsch <mrinsch@myvu.vinu.edu>
2024-09-17 15:26:52 -0400

    Update ModuleInfo.java

--- app/src/main/java/tn/amin/mpro2/constants/ModuleInfo.java
+++ app/src/main/java/tn/amin/mpro2/constants/ModuleInfo.java
@@ -2,13 +2,13 @@ package tn.amin.mpro2.constants;
 
 public class ModuleInfo {
     public static final String LINK_PATREON_PAGE = "https://www.patreon.com/AminGuermazi/";
-    public static final String LINK_GITHUB_PAGE = "https://github.com/Mino260806/MessengerPro/";
+    public static final String LINK_GITHUB_PAGE = "https://github.com/gfunkmonk/MessengerPro/";
     public static final String LINK_TELEGRAM_SUPPORT = "https://t.me/MProSupport/";
     public static final String LINK_GITHUB_WIKI_USAGE_GUIDE = "https://github.com/Mino260806/MessengerPro/wiki/Usage-Guide/";
 
-    public static final int MIN_ORCA_VERSION = 321610181;
-    public static final int MAX_ORCA_VERSION = 321610252;
-    public static final String RECOMMENDED_ORCA_VERSION_STRING = "458.0.0.54.108";
+    public static final int MIN_ORCA_VERSION = 323208846;
+    public static final int MAX_ORCA_VERSION = 323409551;
+    public static final String RECOMMENDED_ORCA_VERSION_STRING = "466.0.0.39.109";
 
     public static final String PACKAGE_AI_PLUGIN = "tn.amin.mproai";
 }
