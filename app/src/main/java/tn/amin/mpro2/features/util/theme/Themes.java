93f275841c23846ff8f8c5a77265d17c2ade6cf3
Matt Rinsch <mrinsch@myvu.vinu.edu>
2024-09-18 04:52:35 -0400

    Update Themes.java

--- app/src/main/java/tn/amin/mpro2/features/util/theme/Themes.java
+++ app/src/main/java/tn/amin/mpro2/features/util/theme/Themes.java
@@ -17,15 +17,17 @@ import tn.amin.mpro2.features.util.theme.supplier.StaticThemeColorSupplier;
 public class Themes {
     public static final ArrayList<ThemeInfo> themes = new ArrayList<>(Arrays.asList(
             new ThemeInfo("None", new KeepThemeColorSupplier()),
-
             new ThemeInfo("Green", new StaticThemeColorSupplier(Color.GREEN)),
-
             new ThemeInfo("Magenta", new StaticThemeColorSupplier(Color.MAGENTA)),
-
-            new ThemeInfo("Teal", new StaticThemeColorSupplier(Color.parseColor("#05998c"))),
-
             new ThemeInfo("Red", new StaticThemeColorSupplier(Color.RED)),
             new ThemeInfo("Yellow", new StaticThemeColorSupplier(Color.YELLOW)),
+
+            new ThemeInfo("Teal", new StaticThemeColorSupplier(Color.parseColor("#05998c"))),
+            new ThemeInfo("Pink", new StaticThemeColorSupplier(Color.parseColor("#ff69b4"))),
+            new ThemeInfo("Orange", new StaticThemeColorSupplier(Color.parseColor("#ffa500"))),
+            new ThemeInfo("Chartreuse", new StaticThemeColorSupplier(Color.parseColor("#dfff00"))),
+            new ThemeInfo("Purple", new StaticThemeColorSupplier(Color.parseColor("#9d00ff"))),
+            new ThemeInfo("Brown", new StaticThemeColorSupplier(Color.parseColor("#b87333"))),
             new ThemeInfo("Custom", new CustomThemeColorSupplier())
 
 //            new ThemeInfo("Custom", null)
